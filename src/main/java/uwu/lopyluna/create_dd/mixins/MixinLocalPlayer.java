a2284a504741024d1a0323d7af947a462971ed61
IThundxr <me@ithundxr.dev>
2024-09-03 19:57:30 -0400

    Switch to Inject for player rideTick

--- src/main/java/uwu/lopyluna/create_dd/mixins/MixinLocalPlayer.java
+++ src/main/java/uwu/lopyluna/create_dd/mixins/MixinLocalPlayer.java
@@ -6,16 +6,16 @@ import net.minecraft.client.player.AbstractClientPlayer;
 import net.minecraft.client.player.Input;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.world.entity.player.ProfilePublicKey;
-import net.minecraft.world.entity.vehicle.Boat;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import uwu.lopyluna.create_dd.content.blocks.contraptions.contraption_block.BoatContraptionEntity;
 
-@Mixin(value = LocalPlayer.class)
+@Mixin(LocalPlayer.class)
 public class MixinLocalPlayer extends AbstractClientPlayer {
-
     @Shadow public Input input;
     @Shadow private boolean handsBusy;
 
@@ -23,23 +23,11 @@ public class MixinLocalPlayer extends AbstractClientPlayer {
         super(pClientLevel, pGameProfile, pProfilePublicKey);
     }
 
-    /**
-     * @author u
-     * @reason u
-     */
-    @Overwrite
-    public void rideTick() {
-        super.rideTick();
-        if (this.wantsToStopRiding() && this.isPassenger()) this.input.shiftKeyDown = false;
-        this.handsBusy = false;
-        if (this.getVehicle() instanceof Boat boat) {
-            boat.setInput(this.input.left, this.input.right, this.input.up, this.input.down);
-            this.handsBusy |= this.input.left || this.input.right || this.input.up || this.input.down;
-        }
-        if (this.getVehicle() instanceof BoatContraptionEntity boat) {
-            boat.setInput(this.input.left, this.input.right);
+    @Inject(method = "rideTick", at = @At("TAIL"))
+    private void create_dd$rideTickForBoatContraptionEntity(CallbackInfo ci) {
+        if (this.getVehicle() instanceof BoatContraptionEntity boatContraption) {
+            boatContraption.setInput(this.input.left, this.input.right);
             this.handsBusy |= this.input.left || this.input.right;
         }
-
     }
 }
